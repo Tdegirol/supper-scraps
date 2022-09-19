c281fdfc8633725f0367236320b83279eb92c078
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-19 10:32:13 -0600

    Added footer and got it to stay at the bottom of the page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,9 +3,9 @@ import { Card } from "react-bootstrap"
 
 const Footer = () => {
   return (
-    <div className="text-center text-light bg-dark">
-      <div>
-        <h1>Why Use This App</h1>
+    <div className="footer text-center text-light bg-dark">
+      <div className="my-4">
+        <h3>Why Use This App</h3>
         <p className='my-4'>
           Spend less money |  Decrease GreenHouse Gases  |  Avoid the grocery store.
         </p>
