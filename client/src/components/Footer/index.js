ace30e57418d344ce51fe6fba1dd9d2de5550086
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-19 10:06:53 -0600

    added short footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import { Button, Card } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <Card className="text-center">
+      <Card.Body>
+        <Card.Title>Why Use This App</Card.Title>
+        {/* <Button variant="primary" className="mx-4">Environmental</Button>
+        <Button variant="primary" className="mx-4">Economic</Button>
+        <Button variant="primary" className="mx-4">Easy</Button> */}
+        <Card.Text className='my-4'>
+        {/* <h1>Search for Recipes!</h1>
+        <h1>Search for Recipes!</h1>
+        <h1>Search for Recipes!</h1> */}
+          Spend less money |  Decrease methane buildup in landfills  |  Avoid the grocery store.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Footer;
