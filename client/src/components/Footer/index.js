d7b71bf125da027552d76f65e3d11ac647003cb0
Samantha-Ruth <sam.haberman@gmail.com>
2022-09-19 10:16:36 -0600

    cleanup code

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import { Button, Card } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 
 const Footer = () => {
   return (
     <Card className="text-center text-light bg-dark">
       <Card.Body>
         <Card.Title>Why Use This App</Card.Title>
-        {/* <Button variant="primary" className="mx-4">Environmental</Button>
-        <Button variant="primary" className="mx-4">Economic</Button>
-        <Button variant="primary" className="mx-4">Easy</Button> */}
         <Card.Text className='my-4'>
-        {/* <h1>Search for Recipes!</h1>
-        <h1>Search for Recipes!</h1>
-        <h1>Search for Recipes!</h1> */}
           Spend less money |  Decrease GreenHouse Gases  |  Avoid the grocery store.
         </Card.Text>
       </Card.Body>
