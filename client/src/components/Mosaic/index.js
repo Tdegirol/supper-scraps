abc0d8416ecf32f669a487e0160fa22aaee1885a
chrispobrien <chris.obrien57@gmail.com>
2022-09-19 14:29:12 -0400

    Added some images to fill 1920x1080 space

--- client/src/components/Mosaic/index.js
+++ client/src/components/Mosaic/index.js
@@ -23,6 +23,13 @@ const Mosaic = () => {
     "16.jpg",
     "17.jpg",
     "18.jpg",
+    "19.jpg",
+    "20.jpg",
+    "21.jpg",
+    "22.jpg",
+    "23.jpg",
+    "24.jpg",
+    "25.jpg"
   ];
 
   return (
